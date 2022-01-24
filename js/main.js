302cc2968551da595672c80997596237de773a2f
denisesenguel <denisesenguel@gmail.com>
2022-01-24 11:40:50 +0100

    add player and collectible to dom upon start of game

--- js/main.js
+++ js/main.js
@@ -2,10 +2,25 @@ class Game {
 
     start() {
         
-        // create basic elements
         this.player = new Player();
         this.collectible = new Collectible();
+
+        console.log(this.collectible);
+        this.addToDOM(this.player);
+        this.addToDOM(this.collectible);
+    }
+
+    addToDOM(object) {
         
+        const newElm = document.createElement('div');
+        
+        newElm.className = object.name;
+        newElm.style.width = object.width + "%";
+        newElm.style.height = object.height + "%";
+        newElm.style.top = object.position.y + "%";
+        newElm.style.left = object.position.x + "%";
+
+        document.getElementById('board').appendChild(newElm);
     }
 
 
@@ -13,6 +28,7 @@ class Game {
 
 class Player {
     constructor() {
+        this.name = 'player';
         this.width = 5;
         this.height = 5;
         this.speed = 1;
@@ -26,11 +42,15 @@ class Player {
 class Collectible {
 
     constructor() {
+        this.name = 'collectible';
         this.width = 5;
-        this.width = 5;
+        this.height = 5;
         this.position = {
             x: 20,
             y: 80
         };
     }
 }
+
+const game = new Game;
+game.start();
